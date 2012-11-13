92f3c17a3d2a3327778eb14f317f7ff5bcd652b2
hschmitz <hschmitz@41cd2dbe-1fc6-41bc-8815-360ab1253c1b>
2012-11-13 17:21:20 +0000

    Added Particle class and ParticleStorage
    git-svn-id: https://svn.code.sf.net/p/opar/code/trunk@11 41cd2dbe-1fc6-41bc-8815-360ab1253c1b

---
+++ src/particles.cpp
@@ -0,0 +1,95 @@
+/*
+ * particles.cpp
+ *
+ * Created on: 13 Nov 2012
+ * Author: Holger Schmitz
+ * Email: holger@notjustphysics.com
+ *
+ * Copyright 2012 Holger Schmitz
+ *
+ * This file is part of OPar.
+ *
+ * OPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OPar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "particles.hpp"
+
+#include <algorithm>
+
+static const long STORAGE_BLOCK_SIZE = 1000000;
+
+DataBlock::DataBlock()
+{
+  data = new Particle[STORAGE_BLOCK_SIZE];
+}
+
+DataBlock::DataBlock(const DataBlock &block) : data(block.data), count(block.count) {}
+
+void DataBlock::free()
+{
+  delete[] data;
+}
+
+Particle &DataBlock::addParticle()
+{
+  return data[count++];
+}
+
+Particle &ParticleStorage::addParticle()
+{
+  // find a free block or create one
+  if ((STORAGE_BLOCK_SIZE == freeBlock->count) || (freeBlock == blocks.end()))
+  {
+    bool foundBlock = false;
+    freeBlock = blocks.begin();
+    while(!foundBlock && (freeBlock != blocks.end()))
+    {
+      foundBlock = (STORAGE_BLOCK_SIZE > freeBlock->count);
+      ++freeBlock;
+    }
+    if (!foundBlock)
+    {
+      freeBlock = blocks.insert(blocks.begin(), DataBlock());
+    }
+  }
+  return freeBlock->addParticle();
+
+}
+
+ParticleStorage::iterator ParticleStorage::removeParticle(const iterator &it_)
+{
+  iterator it = it_;
+  DataBlock &dblock = *(it.blockIter);
+  Particle *data = dblock.data;
+  long pos = it.pos;
+  long last = dblock.count - 1;
+
+  if (last>pos) std::swap(data[pos], data[last]);
+
+  // if no more particles in the block then delete the block
+  if (0 == --dblock.count)
+  {
+    // manually free the data
+    dblock.free();
+    it.blockIter = blocks.erase(it.blockIter);
+    it.pos = 0;
+  }
+
+  // for performance, see if there are more free spaces in this block than in the
+  // freeBlock, and update freeBlock accordingly
+  if ((it.blockIter != blocks.end()) && (it.blockIter->count < freeBlock->count))
+  {
+    freeBlock = it.blockIter;
+  }
+}
---
+++ src/particles.hpp
@@ -0,0 +1,160 @@
+/*
+ * particles.hpp
+ *
+ * Created on: 13 Nov 2012
+ * Author: Holger Schmitz
+ * Email: holger@notjustphysics.com
+ *
+ * Copyright 2012 Holger Schmitz
+ *
+ * This file is part of OPar.
+ *
+ * OPar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OPar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OPar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PARTICLES_HPP_
+#define PARTICLES_HPP_
+
+#include "types.hpp"
+
+#include <list>
+#include <iterator>
+
+
+/** @Particle
+ *  A low impact particle class for holding all the particle data
+ */
+class Particle
+{
+  private:
+    static long nextIndex;
+    long index;
+  public:
+    SVector x;
+    PVector p;
+    real weight;
+
+    Particle() {}
+    Particle(const SVector &x_, const PVector &p_, real weight_)
+      : index(++nextIndex), x(x_), p(p_), weight(weight_) {}
+    Particle(const Particle &P) : index(P.index), x(P.x), p(P.p), weight(P.weight) {}
+    Particle &operator=(const Particle &P)
+    {
+      x = P.x;
+      p = P.p;
+      weight = P.weight;
+      index = P.index;
+    }
+
+    void copyFrom(const Particle &P)
+    {
+      x = P.x;
+      p = P.p;
+      weight = P.weight;
+      index = ++nextIndex;
+    }
+
+    long getIndex() { return index; }
+
+    void setValues(SVector &x_, PVector &p_, real weight_)
+    {
+      x = x_;
+      p = p_;
+      weight = weight;
+    }
+
+    void setValues(SVector &x_, PVector &p_)
+    {
+      x = x_;
+      p = p_;
+    }
+};
+
+
+class ParticleStorage
+{
+  private:
+    struct DataBlock {
+        Particle *data;
+        long count;
+        DataBlock();
+        DataBlock(const DataBlock &block);
+        void free();
+        Particle &addParticle();
+    };
+    typedef std::list<DataBlock> BlockList;
+    typedef BlockList::iterator BlockIterator;
+    BlockList blocks;
+    BlockIterator freeBlock;
+  public:
+    ParticleStorage()
+    {
+      freeBlock = blocks.end();
+    }
+
+    class iterator : public std::iterator<std::forward_iterator_tag, Particle>
+    {
+      private:
+        friend class ParticleStorage;
+        BlockIterator blockIter;
+        long pos;
+
+        iterator(BlockIterator blockIter_, long pos_ = 0) : blockIter(blockIter_), pos(pos_) {}
+      public:
+        iterator() {}
+        iterator(const iterator &it)
+        {
+          blockIter = it.blockIter;
+          pos = it.pos;
+        }
+
+        iterator& operator++()
+        {
+          if (++pos >= blockIter->count)
+          {
+            ++blockIter;
+            pos = 0;
+          }
+          return *this;
+        }
+
+        iterator operator++(int)
+        {
+          iterator tmp(*this);
+          operator++();
+          return tmp;
+        }
+
+        bool operator==(const iterator& rhs) { return (blockIter == rhs.blockIter) && (pos == rhs.pos); }
+        bool operator!=(const iterator& rhs) { return (blockIter != rhs.blockIter) || (pos != rhs.pos); }
+        Particle& operator*() { return blockIter->block[pos]; }
+    };
+
+    iterator begin() { return iterator(BlockList.begin()); }
+    iterator end() { return iterator(BlockList.end()); }
+
+
+    Particle &addParticle();
+
+    /** Remove a particle from the storage.
+     *
+     * After deletion the iterator will point to the position after the deleted particle.
+     * This is in line with STL behaviour
+     */
+    iterator removeParticle(const iterator&);
+
+};
+
+#endif /* PARTICLES_HPP_ */
